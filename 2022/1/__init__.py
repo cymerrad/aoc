b473e51e8dd2c8330a316ba9e470570bb569e6bb
Radosław Cymer <cymerrad@gmail.com>
2022-12-11 20:36:18 +0100

    First task

---
+++ 2022/1/__init__.py
@@ -0,0 +1,14 @@
+from typing import *
+
+
+def parse(input: str):
+    chunks = input.strip().split("\n")
+    return [[int(y) for y in chunk.split()] for chunk in chunks]
+
+
+def solve(data) -> int:
+    return max([sum(chunks) for chunks in data])
+
+def solve_2(data) -> int:
+    decreasing = sorted([sum(chunks) for chunks in data], reverse=True)
+    return sum(decreasing[:3])
